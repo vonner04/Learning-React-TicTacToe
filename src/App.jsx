4ab2d691577c0368e450524aadfdaafdf916e87a
unknown <randyleean092m@gmail.com>
2024-01-11 23:51:41 +1300

    bugFix: grid couldn't populate as x or o
    bug was due to misuse of syntax in setting squares array.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Square from "./Square";
 import { useState } from "react";
 import "./App.css";
 
+const defaultSquares = () => new Array(9).fill(null);
+
 export default function App() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(defaultSquares());
 
   function handleSquareClick(index) {
     let newSquares = squares;
     newSquares[index] = "x";
-    setSquares({ ...newSquares });
+    setSquares([...newSquares]);
   }
 
   return (
@@ -18,11 +20,11 @@ export default function App() {
         <Board>
           {
             //Populate the grid
-            squares.map((square) => (
+            squares.map((square, index) => (
               <Square
                 x={square === "x" ? 1 : 0}
-                o={square === "x" ? 1 : 0}
-                onClick={() => handleSquareClick()}
+                o={square === "o" ? 1 : 0}
+                onClick={() => handleSquareClick(index)}
               />
             ))
           }
