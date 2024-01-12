afc1df912303b6d2b5a97acec5cb23ec9bdbb107
unknown <randyleean092m@gmail.com>
2024-01-12 15:35:42 +1300

    feat: logic for when is player turn
    currently player can only play first and when the amount of filled boxes are even.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ export default function App() {
   const [squares, setSquares] = useState(defaultSquares());
 
   function handleSquareClick(index) {
+    //Check for the amount of filled squares
+    const filledSquares = (square) => square !== null;
+    const isPlayerTurn = squares.filter(filledSquares).length % 2 === 0;
+
+    //Determine if it's the player's turn
+    if (!isPlayerTurn) return;
     let newSquares = squares;
     newSquares[index] = "x";
     setSquares([...newSquares]);
